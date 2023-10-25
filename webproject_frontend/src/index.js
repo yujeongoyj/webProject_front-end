e77831255248901b614c367e30813bfe669f2424
yujeongoyj <oyj3989288@gmail.com>
2023-10-25 16:18:29 +0900

    로그인폼o/회원가입x

--- webproject_frontend/src/index.js
+++ webproject_frontend/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AddProduct from './Projecet_1/pages/AddProduct';
+import Login from './Projecet_1/pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
 
