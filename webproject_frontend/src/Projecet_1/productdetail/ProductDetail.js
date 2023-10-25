3aedd54f54377ac07d6e4a12d25fd2f6296e8a79
01seojin <126742755+01seojin@users.noreply.github.com>
2023-10-26 05:41:06 +0900

    css 얼추 끝

--- webproject_frontend/src/Projecet_1/productdetail/ProductDetail.js
+++ webproject_frontend/src/Projecet_1/productdetail/ProductDetail.js
@@ -1,7 +1,7 @@
 // src/components/ProductDetail.js
 
 import React, {useState} from 'react';
-import Bottom from '../Bottom/Bottom';
+import Header from '../Header/Header';
 import Product from '../Product/Product';
 import './ProductDetail.css';
 import styled from 'styled-components';
@@ -22,42 +22,45 @@ const ProductDetail = (props) => {
         <div className='card'>
           <img  className='photoImg' style={{width:"428px", height:"428px"}} src="https://dnvefa72aowie.cloudfront.net/origin/article/202309/4ae232cb1c73dd693de0f3ce823cea48b10aad0012e37fc891bb6c4f35ee6e81.jpg?q=82&amp;s=300x300&amp;t=crop&amp;f=webp" alt="이미지"/>
         </div>
-        <div className='detailsContainer'>
+        <div className='detailContainer'>
           <div className='detailHeader'>
-            <h2 className='productName'>가방</h2>
+            <div className='productName'>가방</div>
             <h1 className='price'>70,000<span>원</span></h1>
           </div>
           <div className='detailTop'>
-            <div className='like'>
-              <img className='miniLike' style={{width: "16px", height:"16px", marginRight:"3px"}} src={miniheart} alt="미니하트"/>                <p className='likeNumber'>2</p>   
+            <div className='detailLeft'>
+            <div className='box1'>
+              <img className='miniLike' style={{width: "16px", height:"16px", marginRight:"3px"}} src={miniheart} alt="미니하트"/>                
+              <div className='number1'>2</div>   
             </div>
-            <div className='click'>
+            <div className='box1'>
               <img className='miniEyes' style={{width: "21px", height:"13px", marginRight:"3px"}} src={minieyes} alt="미니아이즈"/>
-              <p className='clickNumber'>28</p>
+              <div className='number1'>28</div>
             </div>
-            <div className='time'>
+            <div className='box1'>
               <img className='miniTime' style={{width: "16px", height:"16px", marginRight:"3px"}} src={minitime} alt="미니시계"/>
-              <p className='timeNumber'>2시간전</p>
+              <div className='number1'>2시간전</div>
+            </div>
             </div>
           </div>
           <div className='detailMiddle'>
             <div style={{display:"flex", marginBottom:"20px"}}>
-              <p className='productState' >상품상태</p>
-              <p className='used'>중고</p>
+              <div className='listName' >상품상태</div>
+              <div className='listValue'>중고</div>
             </div>
             <div style={{display:"flex", marginBottom:"20px"}}>
-              <p className='excahnge'>교환여부</p>
-              <p className='exchangeStatus'>교환가능</p>
+              <div className='listName'>교환여부</div>
+              <div className='listValue'>교환가능</div>
             </div>
           </div>
             <div className='detailBottom'>
-              <button type='button' className='button1' >
+              <button type='button' className='button0' >
                 <img className='maxLike' src={whiteheart} alt="하얀색하트"/>
-                <p className='shoppingBasket'>찜</p>
-                <p className='shoppingBasketNumber'>2</p>
+                <span className='shoppingBasket'>찜</span>
+                <span className='shoppingBasket'>2</span>
               </button>
-              <button type='button' className='talk'>대림톡</button>
-              <button type='button' className='buy'>바로구매</button>
+              <button type='button' className='button1'>대림톡</button>
+              <button type='button' className='button2'>바로구매</button>
             </div>
           </div>
         </div>
